2b882e5846c0b2a9f043693db4d50f5fedc02f8d
tinn812 <tin890812@gmail.com>
2025-04-28 15:15:16 +0800

    開啟詳細錯誤頁面

--- DiaryApp/Program.cs
+++ DiaryApp/Program.cs
@@ -62,12 +62,16 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseDeveloperExceptionPage(); // 顯示詳細錯誤頁面
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
